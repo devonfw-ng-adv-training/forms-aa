a8302a01919b008dfb485afa862af14500c05319
Philipp Huber <Philipp.HB.Huber@partner.bmw.de>
2019-12-07 18:17:24 +0100

    added initial state for nested forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { RouterModule } from '@angular/router';
-import { routes } from './shared/routes/routes';
-import { BookModule } from './book/book.module';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+import {RouterModule} from '@angular/router';
+import {routes} from './shared/routes/routes';
+import {BookModule} from './book/book.module';
+import {HttpClientModule} from '@angular/common/http';
+import {OrderModule} from './order/order.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,9 +19,11 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule.forRoot(),
     RouterModule.forRoot(routes),
     // feature modules
-    BookModule
+    BookModule,
+    OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
